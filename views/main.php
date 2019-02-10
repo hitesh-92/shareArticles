fba2de3acc564fda0c7a5ec895d1a249fb8eee7d
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-02-10 18:44:42 +0000

    home and posts pages rendering

--- views/main.php
+++ views/main.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <title>Posts Board</title>
 </head>
 <body>
-    <?php require $view; ?>
+
+    <nav>
+        <p>Posts Board</p>
+        <a href="<?php echo ROOT_URL; ?>">Home</a>
+        <a href="<?php echo ROOT_URL; ?>/posts">Posts</a>
+    </nav>
+    
+    <div class="container">
+        <div class="row"><?php require $view; ?></div>
+    </div>
+    
 </body>
 </html>
